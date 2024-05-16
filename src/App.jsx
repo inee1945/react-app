593e6f019de56530c22a86853f9ecf88162f5d85
inee1945 <inee1945@naver.com>
2024-05-16 10:11:03 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { Children, useState } from "react";
+import { Children, useReducer, useState } from "react";
 import "./App.css";
-import TabMenu from "./TabMenu";
 
-export default function Accordion() {
-  const [activeIndex, setActiveIndex] = useState(1);
-  return (
-    <>
-      <Panel title="JAVA" status={activeIndex === 0}>
-        자바는 어쩌고
-      </Panel>
-      <Panel title="REACT" status={activeIndex === 1}>
-        리엑트는 .....
-      </Panel>
-    </>
-  );
-}
+const counterReducer = (state, action) => {
+  console.log("##########", state.count, action);
+  switch (action) {
+    case "inc":
+      return { count: state.count + 1 };
+    case "dic":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
 
-function Panel({ title, children, status }) {
+export default function Counter() {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+
   return (
-    <section>
-      <h2>{title}</h2>
-      {status ? <p>{children}</p> : <button type="button">열기</button>}
-    </section>
+    <div>
+      <p>Count: {state.count}</p>
+      <button type="buton" onClick={() => dispatch("inc")}>
+        증가
+      </button>
+      <button type="buton" onClick={() => dispatch("dic")}>
+        감소
+      </button>
+    </div>
   );
 }
